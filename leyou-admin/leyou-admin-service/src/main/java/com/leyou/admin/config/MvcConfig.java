c59c36bf5d0da1daf83b54603862e9d555f76276
ZeroWdd <1710031565@qq.com>
2019-12-01 16:53:54 +0800

    取消后台权限

--- leyou-admin/leyou-admin-service/src/main/java/com/leyou/admin/config/MvcConfig.java
+++ leyou-admin/leyou-admin-service/src/main/java/com/leyou/admin/config/MvcConfig.java
@@ -1,12 +1,10 @@
 package com.leyou.admin.config;
 
-import com.leyou.admin.interceptor.CheckInterceptor;
 import com.leyou.common.util.IdWorker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,19 +14,19 @@ public class MvcConfig implements WebMvcConfigurer {
     @Autowired
     private JwtProperties jwtProperties;
 
-    @Bean
-    public CheckInterceptor checkInterceptor() {
-        return new CheckInterceptor(jwtProperties);
-    }
+//    @Bean
+//    public CheckInterceptor checkInterceptor() {
+//        return new CheckInterceptor(jwtProperties);
+//    }
 
     @Bean
     public IdWorker idWorker(){
         return new IdWorker();
     }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(checkInterceptor())
-                .addPathPatterns("/**").excludePathPatterns("/admin/query");
-    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(checkInterceptor())
+//                .addPathPatterns("/**").excludePathPatterns("/admin/query");
+//    }
 }
