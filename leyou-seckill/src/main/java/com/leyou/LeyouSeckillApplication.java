c39f01586dfa1b2a84cbfd5dea834f3ceb6330c1
ZeroWdd <1710031565@qq.com>
2019-12-05 09:05:43 +0800

    bug修复

--- leyou-seckill/src/main/java/com/leyou/LeyouSeckillApplication.java
+++ leyou-seckill/src/main/java/com/leyou/LeyouSeckillApplication.java
@@ -1,10 +1,10 @@
 package com.leyou;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @Auther: wdd
