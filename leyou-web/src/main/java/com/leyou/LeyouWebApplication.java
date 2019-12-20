1fb9ea0b3f0fe9bd045cdcda147a588700afef98
ZeroWdd <1710031565@qq.com>
2019-12-20 16:16:30 +0800

    功能告一段落

--- leyou-web/src/main/java/com/leyou/LeyouWebApplication.java
+++ leyou-web/src/main/java/com/leyou/LeyouWebApplication.java
@@ -14,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 public class LeyouWebApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(LeyouWebApplication.class, args);
     }
